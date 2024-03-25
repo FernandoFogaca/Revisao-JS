4714501ab102a3becc484efc6239a2751d481f63
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-03-26 00:42:33 +0100

    Done commit

--- script.js
+++ script.js
@@ -1,25 +1,59 @@
 
-function executar(){
-/* Pegar o valor do input */
+async function executar() {
+    /* Pegar o valor do input */
 
-/*Fazer chamada para cadastrar livros */
+    /*Fazer chamada para cadastrar livros */
 
-/*mostrar resultado abaixo do form*/
-
-
-const inputTitulo = document.getElementById("inputTitulo")
-const inputDescricao = document.getElementById("inputDescricao")
-
-const titulo = inputTitulo.value 
-const descricao =   inputDescricao.value 
+    /*mostrar resultado abaixo do form*/
 
 
+    const inputTitulo = document.getElementById("inputTitulo")
+    const inputDescricao = document.getElementById("inputDescricao")
 
-const elementoS = document.getElementById("sucesso")
-elementoS.style.display = "block"
+    const titulo = inputTitulo.value
+    const descricao = inputDescricao.value
 
-const elementoE = document.getElementById("erro")
-elementoE.style.display = "block"
+
+    const elementoE = document.getElementById("erro")
+    if (titulo == "" || descricao == "") {
+        elementoE.style.display = "block"
+
+        return // Finaliza a função.
+    }
+
+
+
+    elementoE.style.display = "none"
+    const elementoS = document.getElementById("sucesso")
+    elementoS.style.display = "block"
+
+
+
+    // Fazer uma chamada para cadastrar livros.
+
+    const url = "https://api-aula.up.railway.app/livros"
+
+    const payload = {
+        title: titulo,
+        description: descricao
+    }
+    const fetchOptions = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        Headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    const response = await fetch(url, fetchOptions)
+    const data = await response.json()
+
+    const elementoResultado = document.getElementById("resultado")
+    elementoResultado.innerHTML = data
+
+
+
 
 
 }
+

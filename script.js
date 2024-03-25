f885a702b845e462ed6286afcb2f6cab69a830bf
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-03-25 21:07:04 +0100

    Updated

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function executar(){
+/* Pegar o valor do input */
+
+/*Fazer chamada para cadastrar livros */
+
+/*mostrar resultado abaixo do form*/
+
+
+const inputTitulo = document.getElementById("inputTitulo")
+const inputDescricao = document.getElementById("inputDescricao")
+
+const titulo = inputTitulo.value 
+const descricao =   inputDescricao.value 
+
+
+
+const elementoS = document.getElementById("sucesso")
+elementoS.style.display = "block"
+
+const elementoE = document.getElementById("erro")
+elementoE.style.display = "block"
+
+
+}
